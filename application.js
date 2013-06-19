fde429f77fc41e8b8d3e2c0433c15da2c4b73cc5
khrome <abbey@khrome.net>
2013-06-19 03:47:54 -0700

    updating blank as syntax changes

--- application.js
+++ application.js
@@ -1,13 +1,7 @@
-var ext = require('prime-ext');
-var prime = ext(require('prime'));
-var fn = require('prime/es5/function');
 var Protolus = require('protolus');
-require('protolus-resource/handler-less');
-
+Protolus.Resource.enable('less');
 Protolus.internalRequire(require);
 
 var application = Protolus.PanelServer();
-application.openSocket(7734, function(socket){
-    
-});
+//application.openSocket(7734, function(socket){ });
 application.start();
